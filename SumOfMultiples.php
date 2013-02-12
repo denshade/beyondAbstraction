5c105e25d38242abd5d5d66ff4f1e5455522afae
denshade <lieven@showpad.com>
2013-02-12 13:32:51 +0100

    More constants!

--- SumOfMultiples.php
+++ SumOfMultiples.php
@@ -8,12 +8,16 @@ class SumOfMultiples
 {
     const BEGIN = 1;
     const END   = 1000;
+
+    const THREE = 3;
+    const FIVE  = 5;
+
     public function calculate()
     {
         $sum = 0;
         for ($current = self::BEGIN; $current < self::END; $current++ )
         {
-            if ($current % 3 == 0 || $current % 5 == 0)
+            if ($current % self::THREE == 0 || $current % self::FIVE == 0)
             {
                 $sum += $current;
             }
